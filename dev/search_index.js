325e5e2145929c10bb9b1bf923955f790b6831d6
Documenter.jl <documenter@juliadocs.github.io>
2020-10-17 16:24:01 +0000

    build based on f076a2c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Metida","page":"Home","title":"Metida","text":"","category":"section"},{"location":"#Mixed-Models","page":"Home","title":"Mixed Models","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Metida.jl is a Julia package for fitting mixed-effects models. At this moment package is in development stage. ","category":"page"}]
+}
