c4945455023129adf9a09135534a14fc5a260837
PharmCat <v.s.arnautov@yandex.ru>
2021-03-14 21:52:25 +0300

    update

--- src/fvalue.jl
+++ src/fvalue.jl
@@ -1,6 +1,13 @@
 # fvalue.jl
 
+#=
+Metida.fvalue(lmm, [0 0 1 0 0 0; 0 0 0 1 0 0; 0 0 0 0 1 0])
+=#
+"""
+    fvalue(lmm::LMM, l::Matrix)
 
+F value for contrast matrix l.
+"""
 function fvalue(lmm::LMM, l::Matrix)
     lcl = l*lmm.result.c*l'
     f   = lmm.result.beta'*l'*pinv(lcl)*l*lmm.result.beta/rank(lcl)
